54c39536280fd3f3eb6d05728dc4c788a3be39ca
dcs <3257013972@qq.com>
2023-12-02 16:33:43 +0800

    judge login second commit

--- src/main/java/com/bluemsun/controller/JudgeController.java
+++ src/main/java/com/bluemsun/controller/JudgeController.java
@@ -2,6 +2,8 @@ package com.bluemsun.controller;
 
 import com.bluemsun.entity.Judge;
 import com.bluemsun.entity.dto.ResultDto;
+import com.bluemsun.service.JudgeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,11 +13,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/judge")
 public class JudgeController {
+    @Autowired
+    JudgeService judgeService;
 
     @RequestMapping("/login")
     public ResultDto<Object> login(@RequestBody Judge judge){
         ResultDto<Object> rt = new ResultDto<>();
-
+        boolean success = judgeService.login(judge);
+        rt.setResult(success);
+        rt.setMsg(success ? "登录成功" : "登录失败");
         return rt;
     }
 }
