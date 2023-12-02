843a8c4b7ab3de3f56caee4f8c71de3ab202e1b8
dcs <3257013972@qq.com>
2023-12-02 16:52:09 +0800

    judge login third commit

--- src/main/java/com/bluemsun/controller/JudgeController.java
+++ src/main/java/com/bluemsun/controller/JudgeController.java
@@ -4,24 +4,35 @@ import com.bluemsun.entity.Judge;
 import com.bluemsun.entity.dto.ResultDto;
 import com.bluemsun.service.JudgeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 /**
  * 评委
  */
-
+@RestController
+@CrossOrigin
 @RequestMapping("/judge")
 public class JudgeController {
     @Autowired
     JudgeService judgeService;
 
     @RequestMapping("/login")
-    public ResultDto<Object> login(@RequestBody Judge judge){
+    public ResultDto<Object> login(@RequestBody Judge judge, HttpServletRequest request){
         ResultDto<Object> rt = new ResultDto<>();
         boolean success = judgeService.login(judge);
         rt.setResult(success);
         rt.setMsg(success ? "登录成功" : "登录失败");
+
+        if(success){
+            HttpSession session = request.getSession();
+            session.setAttribute("judge", judge);
+        }
         return rt;
     }
 }
